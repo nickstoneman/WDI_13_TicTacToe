a0cb039e4eed50639c12f81437714fcd5acaac4c
Nick Stoneman <nick.stoneman@gmail.com>
2015-05-15 11:29:26 +0100

    changed play-by-play in game making it easier to read

--- script/jqueryscript.js
+++ script/jqueryscript.js
@@ -3,12 +3,17 @@ var moves = 0;
 var gameBoard = [];
 var winningIndexPositions = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2] ];
 
-var adjectives = ['Sweet move for ', 'Nice reply by ', 'Snap! With an ', 'Plays ', 'Lays down an ', 'Players gonna play an ', 'xoxOXoXOOXooo it\'s an... '];
+var adjectives = ['Sweet move for ', 'Nice reply by ', 'Snap! With an ', 'Plays ', 'Lays down an ', 'Players gonna play... an ', 'xoxOXoXOOXooo it\'s an... '];
+
+var playerX = "X";
+var playerO = "O";
+var playerTurn = "";
 
 $( document ).ready(function() {
   console.log( "ready!" );
   $('body').css('background-image', 'url(images/grey_tile.png)');
 
+// Soundmanager initialized
     soundManager.setup({
     url: 'swf/',
     flashVersion: 9,
@@ -16,50 +21,41 @@ $( document ).ready(function() {
     onready: function() {
     }
   });
-      $('.box').on("click", function(){
+  
+// Sound on board click
+  $('.box').on("click", function(){
       var mySound = soundManager.createSound({
       url: 'audio/tommib_cutA.mp3'
           });
       mySound.play()
        })
    
-  // Store the player moves as X or O in array
-  // var gameBoard = [ [0, 0, 0], [0, 0, 0], [0, 0, 0] ];
-  // Winning indexPositions. If x === winCombos or o === winCombos gets winner. 
+// START GAME FUNCTION / COIN TOSS has been removed
+// Need a playerTurn function to toggle between player moves
+// cointToss selects first turn of X or O
+// var coinToss = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+// console.log(coinToss);
 
-    var playerX = "X";
-    var playerO = "O";
-    var playerTurn = "";
-
-  // START GAME FUNCTION / COIN TOSS
-  // Need a playerTurn function to toggle between player moves
-  // cointToss selects first turn of X or O
-  // var coinToss = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
-  // console.log(coinToss);
-
-  // Sets first move to "X"
+// Sets first move to "X"
     var playerTurn = function() {
       if (moves % 2 === 0) {
         playerTurn = playerX;
-        // console.log(playerTurn);
       } else {
         playerTurn = playerO;
       }
    };
     
-   // Change turns- X goes first- then logs move to gameBoard[]
+// Change turns- X goes first- then logs move to gameBoard[]
     $(".box").on("click", function(e) {
       if (moves % 2 === 0) {
         // console.log("move is even");
         // debugger
-        // $('.box').text('X');
         this.childNodes[3].innerText = "X"
         var id = Number(this.id);
         gameBoard[id] = "X";
-        
         var output = this.childNodes[3].innerText = "X"; 
         var randomAdjective = adjectives[Math.floor(adjectives.length * Math.random())];
-        $('ul').append('<li>' + randomAdjective + " " + output + '</li>');
+        $('#playByPlay').append('<h4>' + "Player 1: " + randomAdjective + " " + output + '</h4>');
         // playerXArray[id] = "X";
         // gameBoard.push("x");
       } else {
@@ -72,7 +68,7 @@ $( document ).ready(function() {
         // console.log(playerOArray);
         var output = this.childNodes[3].innerText = "O"; 
         var randomAdjective = adjectives[Math.floor(adjectives.length * Math.random())];
-        $('ul').append('<li>' + randomAdjective + " " + output + '</li>');
+        $('#playByPlay').append('<h4>' + "Player 2: " + randomAdjective + " " + output + '</h4>');
       }
       
       // Decide who wins
